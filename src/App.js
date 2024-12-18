ceea88070330c40cf90750b3acfe5347b8ba4d4d
Caiorossi00 <caio_rossi56@hotmail.com>
2024-12-18 12:01:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="ui-left">
+        <Header />
+      </div>
+      <div id="ui-right">Projects</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../assets/css/Header.scss";
+
+const Header = () => {
+  return (
+    <div id="container-header">
+      <div id="header-left">
+        <p>Logo</p>
+        <h1>Caiorossi.dev</h1>
+      </div>
+      <button>My Works</button>
+    </div>
+  );
+};
+
+export default Header;
