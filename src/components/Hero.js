5d06df9aef7a4ae0fe7644bf9ee60154e37bb508
Caiorossi00 <caio_rossi56@hotmail.com>
2024-12-18 20:46:31 -0300

    ajustes css + hero pronta

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,12 +7,12 @@ const Hero = () => {
     <div id="container-hero">
       <div className="hero-text">
         <p>
-          Hey, I'm Caio Rossi and I love playing around with shapes and colours
-          on the internet, making things look shiny and cool!
+          Hey, I'm <span>Caio Rossi</span> and I love playing around with shapes
+          and colours on the internet, making things look shiny and cool!
         </p>
-        <div>
-          <button>Lorem</button>
-          <button>Lorem</button>
+        <div id="button-hero-div">
+          <button>Contact Me</button>
+          <button>Contact Me</button>
         </div>
       </div>
       <img src={logo} alt="Caiorossi.dev Logo"></img>
