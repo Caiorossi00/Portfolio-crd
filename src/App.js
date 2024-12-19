2de7b6ef0f715ecfd75114f984cae5aabf35166a
Caiorossi00 <caio_rossi56@hotmail.com>
2024-12-18 21:50:26 -0300

    skills estruturado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Education from "./components/Education";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <div id="ui-left-bottom">
           <Education />
+          <Skills />
         </div>
       </div>
       <div id="ui-right">Projects</div>
---
+++ src/components/Skills.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../assets/css/Skills.scss";
+
+const Skills = () => {
+  return <div id="container-skills">Skills</div>;
+};
+
+export default Skills;
